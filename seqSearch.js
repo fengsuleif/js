88837edd8e2da1eaf2c9d89e956171b19cc061b0
fengsuleif <fengsuleif@gmail.com>
2014-08-22 16:03:16 +0800

    加入返回值是索引的版本

--- seqSearch.js
+++ seqSearch.js
@@ -6,3 +6,13 @@ function seqSearch(arr,item){
 	}
 	return false;
 }
+
+
+function seqSearch_index(arr,item){
+	for(var i=0;i<arr.length;++i){
+		if(arr[i]==item){
+			return i;
+		}
+	}
+		return -1;
+}
