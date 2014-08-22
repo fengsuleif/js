4dc0048f54c783d9b584f9546fa5490d3f15be1b
fengsuleif <fengsuleif@gmail.com>
2014-08-22 16:40:42 +0800

    查找最大最小值

---
+++ min_max.js
@@ -0,0 +1,18 @@
+
+function findMin(arr){
+	var min=arr[0];
+	for(var i=1;i<arr.length;++i){
+		if(arr[i]<min){
+		min=arr[i];
+		}
+	return min;
+}
+
+function findMax(arr){
+	var max=arr[0];
+	for(var i=1;i<arr.length;++i){
+		if(arr[i]>max){
+		max=arr[i];
+		}
+	return max;
+}
